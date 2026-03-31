90b260b7faea6bef636569e08f9ab7cec3fafeb6
Javed Khan <javed.khan@kanishkasoftware.com>
2026-03-31 17:40:53 +0530

    resume updated

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,6 +1,8 @@
 class AppAssets {
   AppAssets._();
+
   static const String _basePath = 'assets/images';
   static const String profileImage = '$_basePath/profile_image.jpg';
-  static const String resume = 'assets/docs/Mohd_Javed_Khan_Flutter_Dev_Resume_3yoe.pdf';
+  static const String resume =
+      'assets/docs/Mohd_Javed_Khan_Flutter_Dev_Resume_3yoe.pdf';
 }
